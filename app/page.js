d3e1040413a88ba3d6c056563921a0ad506e4e02
ajdinl <ajdinl@live.com>
2023-10-22 12:30:25 +0200

    rename sidemenu to sidebar

--- app/page.js
+++ app/page.js
@@ -1,11 +1,11 @@
 import ChatContent from './_components/ChatContent'
 import Footer from './_components/Footer'
-import Sidemenu from './_components/Sidemenu'
+import Sidebar from './_components/Sidebar'
 
 export default function Home() {
   return (
     <main className='flex min-h-screen flex-row items-center justify-between'>
-      <Sidemenu />
+      <Sidebar />
       <div className='flex flex-col flex-1 items-center'>
         <ChatContent />
         <Footer />
